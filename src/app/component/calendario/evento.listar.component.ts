ebf4fee85157deeb3afc706e507bdc1318285d2e
willian <williandeob@gmail.com>
2017-11-27 00:59:11 -0200

    tela de listagens dos eventos

--- src/app/component/calendario/evento.listar.component.ts
+++ src/app/component/calendario/evento.listar.component.ts
@@ -1,9 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+import { Event } from '../../model/Event';
 
 @Component({
 	selector: 'my-app',
 	templateUrl: 'app/views/calendario/evento.listar.html'
 })
-export class EventoListarComponent  {
+export class EventoListarComponent implements OnInit{
+	Object = Object;
+	dataIni:Date;
+	dataFim:Date;
+	private url: string;
+	events: Map<string, Event>;
+
+	constructor(private http: Http){
+		this.dataIni = new Date();
+		this.dataFim = new Date();
+		this.url = 'http://int02-e07d3.firebaseio.com/calendar/events.json';
+		this.events = new Map<string, Event>();
+	}
+
+	public getEvents(){
+		this.http.get(this.url)
+		.subscribe(data => {
+			this.events = JSON.parse(data['_body']);
+		})
+	}
+
+	ngOnInit() {
+     this.getEvents();
+  }
 
 }
